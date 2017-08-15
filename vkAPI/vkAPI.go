6154afb033edf5efb46bf85c325cccb522e8f643
dnnd <ddann9@live.com>
2017-08-15 18:21:59 +0300

    wall.get +

--- vkAPI/vkAPI.go
+++ vkAPI/vkAPI.go
@@ -5,19 +5,91 @@ import (
     "os/exec"
     "runtime"
     "fmt"
+    "net/http"
+    "bytes"
+    "io/ioutil"
+    "encoding/json"
+    "os"
+    "log"
+    "strconv"
 )
 
-const VKAuthUrl = "https://oauth.vk.com/authorize?"
+const VKAuthUrl= "https://oauth.vk.com/authorize?"
+const VKApiUrl = "https://api.vk.com/method/"
+
+
+type Method struct {
+    object string
+    action string
+}
+
+func accessToken() string{
+    return os.Getenv("VKUSERTOKEN")
+}
+
+func (m Method) Encode(apiUrl string, params *url.Values) (string, error){
+    u, err := url.Parse(apiUrl)
+    if err != nil{
+        return "", err
+    }
+    var buffer bytes.Buffer
+    buffer.WriteString(u.String())
+    buffer.WriteString(m.object)
+    buffer.WriteString(m.action)
+    buffer.WriteString("?")
+    buffer.WriteString(params.Encode())
+    return buffer.String(), nil
+}
+
+func GetWallPosts(owner_id int, offset int, count int, filter string, extendedFields string) (WallItems, error){
+    params := url.Values{}
+    params.Add("access_token", accessToken())
+    params.Add("owner_id", strconv.Itoa(-owner_id))
+    params.Add("offset", strconv.Itoa(offset))
+    params.Add("count",strconv.Itoa(count))
+    params.Add("v","5.67")
+    params.Add("filter",filter)
+    if extendedFields != ""{
+        params.Add("extended", "1")
+        params.Add("extended_fields",extendedFields)
+    }else{
+        params.Add("extended", "0")
+    }
+    response := <-Request(Method{"wall",".get"}, &params)
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil{
+        return WallItems{}, err
+    }
+    var wallResponse WallResponse
+    err = json.Unmarshal(body,&wallResponse)
+    return wallResponse.Response.Items, err
+}
+
+func Request(method Method, params *url.Values) chan *http.Response {
+    reqUrl, _ := method.Encode(VKApiUrl, params)
+    response := make(chan *http.Response, 1)
+    go func (u string, ch chan *http.Response){
+        log.Println(u)
+        r, err := http.Get(u)
+        if err != nil {
+            log.Println(err)
+        }
+        response <- r
+    }(reqUrl, response)
+    return response
+}
 
 func UserImplicitFlow(scope ...int) error {
     params := url.Values{}
     params.Add("client_id", "6148845")
     params.Add("redirect_uri", "https://oauth.vk.com/blank.html")
-    encodedMask := encodeScope (scope...)
+    encodedMask := encodeScope(scope...)
+    log.Println(encodedMask)
     params.Add("scope", encodedMask)
     params.Add("response_type", "token")
     params.Add("display", "page")
     authUrl := VKAuthUrl + params.Encode()
+
     err := browserOpen(authUrl)
     if err != nil {
         return err
@@ -25,18 +97,17 @@ func UserImplicitFlow(scope ...int) error {
     return nil
 }
 
-func GroupImplicitFlow(userID int, scope ...int)  error {
+func GroupImplicitFlow(userID int, scope ...int) error {
     params := url.Values{}
     params.Add("client_id", "6148845")
     params.Add("redirect_uri", "https://oauth.vk.com/blank.html")
     params.Add("response_type", "token")
     params.Add("display", "page")
-    encodedMask := encodeScope (scope...)
+    encodedMask := encodeScope(scope...)
     params.Add("scope", encodedMask)
     err := browserOpen(VKAuthUrl + params.Encode())
     return err
 }
-
 
 func browserOpen(URL string) (err error) {
     err = nil
@@ -55,8 +126,9 @@ func browserOpen(URL string) (err error) {
 
 func encodeScope(scope ...int) string {
     total := 0
-    for i := range scope {
-        total += i
+    for _, val := range scope {
+        total += val
     }
-    return string(total)
+
+    return strconv.Itoa(total)
 }
