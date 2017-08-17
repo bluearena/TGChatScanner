67589aa1a0c792b50bbd28418243c44dab4556e0
zwirec <zwirec@icloud.com>
2017-08-17 18:05:46 +0300

    Add HTTPS

--- service/service.go
+++ service/service.go
@@ -11,6 +11,8 @@ import (
 	"os/signal"
 	"syscall"
 	"log"
+	"crypto/tls"
+	"github.com/kabukky/httpscerts"
 )
 
 type Config map[string]map[string]interface{}
@@ -24,7 +26,7 @@ type Service struct {
 }
 
 func NewService() *Service {
-	return &Service{rAPIHandler: requestHandler.NewRequestHandler(), mux:http.NewServeMux()}
+	return &Service{rAPIHandler: requestHandler.NewRequestHandler(), mux: http.NewServeMux()}
 }
 
 func (s *Service) Run() error {
@@ -38,15 +40,30 @@ func (s *Service) Run() error {
 	if err := s.parseConfig(configPath); err != nil {
 		return err
 	}
-	s.srv = &http.Server{Addr: ":" + s.config["server"]["port"].(string), Handler:s.rAPIHandler}
-	//s.signalProcessing()
+
+	s.signalProcessing()
+
+	err = httpscerts.Check("cert.pem", "key.pem")
+	// If they are not available, generate new ones.
+	if err != nil {
+		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:"+ s.config["server"]["port"].(string))
+		if err != nil {
+			log.Fatal("Error: Couldn't create https certs.")
+		}
+	}
+
+	cer, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+
+	config := &tls.Config{Certificates: []tls.Certificate{cer}}
+
+	s.srv = &http.Server{Addr: ":" + s.config["server"]["port"].(string), Handler: s.rAPIHandler, TLSConfig:config}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	go func() {
 		//defer wg.Done()
-		if err := s.srv.ListenAndServe( /*":" + s.config["server"]["port"].(string), mux*/); err != nil {
+		if err := s.srv.ListenAndServeTLS("cert.pem", "key.pem"); err != nil {
 			wg.Done()
 		}
 	}()
@@ -78,11 +95,11 @@ func (s *Service) signalProcessing() {
 	go s.handler(c)
 }
 
-func (s Service) handler(c chan os.Signal) {
+func (s *Service) handler(c chan os.Signal) {
 	for {
 		<-c
 		log.Print("Gracefully stopping...")
-		s.srv.Close()
+		s.srv.Shutdown(nil)
 		os.Exit(0)
 	}
 }
