e9e8d8b0e872d62c0b6571169d21dab331ec5365
Dnnd <ddann9@live.com>
2017-08-22 23:36:02 +0300

    webhook logging is ok

--- requestHandler/webhook.go
+++ requestHandler/webhook.go
@@ -3,7 +3,6 @@ package requestHandler
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/rs/xid"
 	"github.com/zwirec/TGChatScanner/TGBotApi"
 	"github.com/zwirec/TGChatScanner/models"
@@ -15,26 +14,37 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"errors"
 )
 
 const (
 	UserStatsUrl = "/stats"
 )
 
+var (
+	ErrUnexpectedCommand = errors.New("unexpected command")
+)
+
 func BotUpdateHanlder(w http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
-	logger := req.Context().Value(sysLoggerKey).(*log.Logger)
+	acc_l := req.Context().Value(accLoggerKey).(*log.Logger)
+	sys_l := req.Context().Value(sysLoggerKey).(*log.Logger)
 	if err != nil {
-		logger.Printf("Error during handling request on %s : %s", req.URL.String(), err)
+		sys_l.Printf("Error during reading bot request: %s", err)
+		logHttpRequest(acc_l, req, http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	var update TGBotApi.Update
 	err = json.Unmarshal(body, &update)
 	if err != nil {
-		logger.Printf("Error during unmarshaling request on %s : %s", req.URL.String(), err)
+		sys_l.Printf("Error during unmarshaling request: %s", req.URL.String(), err)
+		logHttpRequest(acc_l, req, http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	var message *TGBotApi.Message
 
 	if update.Message != nil {
@@ -65,34 +75,38 @@ func BotUpdateHanlder(w http.ResponseWriter, req *http.Request) {
 		}
 		appContext.DownloadRequests <- fb
 	} else if len(message.Entities) != 0 && message.Entities[0].Type == "bot_command" {
-		if err := BotCommandRouter(update.Message, logger); err != nil {
-			logger.Println(err)
+		if err := BotCommandRouter(update.Message); err != nil {
+			sys_l.Printf("Command: %s, error: %s", err)
+			if err == ErrUnexpectedCommand {
+				logHttpRequest(acc_l, req, http.StatusOK)
+				w.WriteHeader(http.StatusOK)
+			} else {
+				logHttpRequest(acc_l, req, http.StatusInternalServerError)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
 			return
 		}
 	}
 	w.WriteHeader(http.StatusOK)
 }
 
-func BotCommandRouter(message *TGBotApi.Message, logger *log.Logger) error {
+func BotCommandRouter(message *TGBotApi.Message) error {
 	r := regexp.MustCompile(`\/(start(?:group)?|mystats)?\s*`)
 	command := r.FindStringSubmatch(message.Text)
 	if len(command) == 0 {
-		return fmt.Errorf("unexpected command %s", message.Text)
+		return ErrUnexpectedCommand
 	}
 	switch command[1] {
 	case "start":
 	case "startgroup":
 		err := AddSubsription(&message.From, &message.Chat)
-		if err != nil {
-			return err
-		}
 		return err
 	case "mystats":
 		token, err := SetUserToken(message.From.Id)
-
 		if err != nil {
 			return err
 		}
+
 		us := BuildUserStatUrl(token)
 		_, err = appContext.BotApi.SendMessage(message.Chat.Id, us, true)
 		if err != nil {
